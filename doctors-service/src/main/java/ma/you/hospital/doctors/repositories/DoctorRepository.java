448fb7e033a37aad0def920487ad1d2b0e66a015
obx <othmanebenhalima2@gmail.com>
2025-12-22 20:24:31 +0100

    back end finish

--- doctors-service/src/main/java/ma/you/hospital/doctors/repositories/DoctorRepository.java
+++
@@ -1,6 +0,0 @@
-package ma.you.hospital.doctors.repositories;
-
-import ma.you.hospital.doctors.domain.Doctor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DoctorRepository extends JpaRepository<Doctor, Long> {}
